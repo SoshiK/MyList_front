e4f739cb3cfe5a753b299931ec97667db4f9a2cf
Soshi <SoshiK@users.noreply.github.com>
2021-02-05 11:46:19 +0900

    added tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
 const axios = require("axios");
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
@@ -8,7 +7,6 @@ export default function Home() {
   const router = useRouter();
   const login = () => {
     const text = document.getElementById("text");
-    console.log(process.env.NEXT_PUBLIC_PASSWORD);
     if (text.value === process.env.NEXT_PUBLIC_PASSWORD) {
       router.push("list");
     }else {
@@ -20,14 +18,32 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.container}>
+    <div className="flex py-0 px-0 flex-col justify-center items-center">
       <Head>
         <title>MyList</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewpoint" content="width=device-width, initial-scale=1"></meta>
       </Head>
-      <div>
-        <input type="text" id="text"></input>
-        <button onClick={login}>Login</button>
+      <div className="bg-blue-600 w-full h-16 flex items-center justify-center">
+        <p className="text-center text-white font-serif">Koike Memo</p>
+      </div>
+      <div className="flex flex-col mt-10">
+        <div className="w-full mb-4">
+          <input 
+            type="password" 
+            id="text"
+            className="font-serif text-center appearance-none rounded shadow p-3 text-grey-dark mr-2 focus:outline-none"
+            >
+          </input>
+        </div>
+        <div className="mx-auto">
+          <button 
+            className="inline-block px-6 py-2 text-xs font-serif leading-6 text-center text-white uppercase transition bg-blue-600 rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+            onClick={login}
+            >
+            Login
+          </button>
+        </div>
       </div>
     </div>
   )
