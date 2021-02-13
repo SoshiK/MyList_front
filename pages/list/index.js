59e12a6b7c6e8dd9c6ef5acfb4d83bd1c7389e64
Soshi <SoshiK@users.noreply.github.com>
2021-02-13 23:07:27 +0900

    +ボタンでcreateListページへ遷移

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import AllList from "../../components/AllList";
 const axios = require("axios");
@@ -6,6 +7,7 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function List() {
   const [lists, setLists] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     axios.get(`${BACKEND_URL}/api/list`)
       .then((res) => {
@@ -13,6 +15,9 @@ export default function List() {
         setLists(res.data.result);
       })
   },[])
+  const createList = () => {
+    router.push("list/create");
+  }
   return(
     <div className="flex py-0 px-0 flex-col justify-center items-center">
       <Head>
@@ -25,6 +30,7 @@ export default function List() {
       <div className="mt-3">
         <button
           className="px-6 py-2 text-xs font-serif leading-6 text-center text-white uppercase transition bg-blue-600 rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+          onClick={createList}
         >+</button>
       </div>
       <AllList allList = {lists}/>
