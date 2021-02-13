6ed319f82df62763f6549bb92d2052707584f34d
Soshi <SoshiK@users.noreply.github.com>
2021-02-13 23:07:01 +0900

    listCreateページの準備

---
+++ pages/list/create.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function CreateList() {
+  return (
+    <div>
+      <Head>
+      <title>MyList</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        create
+      </div>
+    </div>
+  );
+}
