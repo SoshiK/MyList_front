4fbb62c3a169d440c1e4e6ce28cd6271f4945a5b
Soshi <SoshiK@users.noreply.github.com>
2021-02-16 17:13:17 +0900

    see detail でidを送るように修正

--- components/ListCard.js
+++ components/ListCard.js
@@ -1,18 +1,31 @@
+import { useRouter } from "next/router";
+
 export default function ListCard(props) {
+  const router = useRouter();
+  const click = () => {
+    router.push({
+      pathname: "/item",
+      query: {
+        id: props.id
+      }
+    })
+  }
   return (
-    <div class="w-4/6 mb-4">
-      <div class="overflow-hidden shadow-md">
-        <div class="px-6 py-4 bg-white font-bold font-serif uppercase">
+    <div className="w-4/6 mb-4">
+      <div className="overflow-hidden shadow-md">
+        <div className="px-6 py-4 bg-white font-bold font-serif uppercase">
           {props.title}
         </div>
-        <div class="px-6 pb-6 bg-white border-b border-gray-200">
+        <div className="px-6 pb-6 bg-white border-b border-gray-200">
           {props.description}
         </div>
-        <div class="p-3 bg-white border-gray-200 text-right">
-        <a class="bg-blue-600 shadow-md text-xs text-white font-serif py-2 px-4 hover:bg-blue-800 rounded uppercase" 
-                href="#">
-                See Detail
-        </a>
+        <div className="p-3 bg-white border-gray-200 text-right">
+        <button 
+          className="bg-blue-600 shadow-md text-xs text-white font-serif py-2 px-4 hover:bg-blue-800 rounded uppercase" 
+          onClick={click}
+        >
+          See Detail
+        </button>
         </div>
       </div>
     </div>
